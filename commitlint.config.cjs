6e23183e4f43aaaeaafa828d54326d5ce3ebb970
Agata <agatadicalogero@gmail.com>
2025-10-09 12:56:42 +0200

    chore: clean up husky scripts and add commitlint configuration

---
+++ commitlint.config.cjs
@@ -0,0 +1,7 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  // optional: allow empty body/subject checks if needed
+  // rules: {
+  //   'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'perf', 'test', 'chore']],
+  // }
+};
